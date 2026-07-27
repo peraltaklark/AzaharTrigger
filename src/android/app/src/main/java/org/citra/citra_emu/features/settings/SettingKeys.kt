87b3750388f5fcb07f19a9a21e7ab1202a523c19
peraltaklark <peraltaklark@gmail.com>
2026-07-27 20:12:42 +0800

    Update SettingKeys.kt

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/SettingKeys.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/SettingKeys.kt
@@ -15,6 +15,8 @@ object SettingKeys {
     external fun is_new_3ds(): String
     external fun lle_applets(): String
     external fun deterministic_async_operations(): String
+    external fun reduce_downcount_slice(): String
+    external fun priority_boost_starved_threads(): String
     external fun enable_required_online_lle_modules(): String
     external fun use_virtual_sd(): String
     external fun compress_cia_installs(): String
