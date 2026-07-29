117e955cff05b121f9dbb326e78294af43fb8bf7
peraltaklark <peraltaklark@gmail.com>
2026-07-29 12:05:31 +0800

    Update NetPlayManager.kt

--- src/android/app/src/main/java/org/citra/citra_emu/utils/NetPlayManager.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/NetPlayManager.kt
@@ -35,6 +35,7 @@ object NetPlayManager {
         stopLANProcessing()
         melonLANEndSession()
         netPlayLeaveRoom()
+        notifyMessageListeners(NetPlayStatus.ROOM_IDLE, "")
     }
     external fun netPlayIsModerator(): Boolean
     external fun netPlayGetBanList(): Array<String>
