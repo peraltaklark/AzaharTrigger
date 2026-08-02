0a16b367850bf2e3e5da87a66b00945826ebf48b
peraltaklark <peraltaklark@gmail.com>
2026-08-03 02:19:19 +0800

    Remove SetProbeResponseParam function
    Removed the SetProbeResponseParam function and its documentation.

--- src/core/hle/service/nwm/nwm_uds.h
+++ src/core/hle/service/nwm/nwm_uds.h
@@ -479,18 +479,6 @@ private:
      */
     void DecryptBeaconData(Kernel::HLERequestContext& ctx);
 
-    /**
-     * NWM_UDS::SetProbeResponseParam service function.
-     * Sets the probe response parameters for the network.
-     *  Inputs:
-     *      1 : Probe OUI
-     *      2 : Probe data
-     *  Outputs:
-     *      0 : Return header
-     *      1 : Result of function, 0 on success, otherwise error code
-     */
-    void SetProbeResponseParam(Kernel::HLERequestContext& ctx);
-
     void CheckSpoofFriendCodeSeed(Kernel::HLERequestContext& ctx, NodeInfo& node);
 
     ResultVal<std::shared_ptr<Kernel::Event>> Initialize(
@@ -671,9 +659,6 @@ private:
 
     // List of the last <MaxBeaconFrames> beacons received from the network.
     std::list<Network::WifiPacket> received_beacons;
-
-    u32 probe_oui = 0;
-    u8 probe_data = 0;
 
     template <class Archive>
     void serialize(Archive& ar, const unsigned int);
