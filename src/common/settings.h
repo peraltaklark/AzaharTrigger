237e372fe91bf879e7e18b1f50166a2220e5dea1
peraltaklark <peraltaklark@gmail.com>
2026-08-03 05:50:27 +0800

    Add use_fastmem setting to settings.h

--- src/common/settings.h
+++ src/common/settings.h
@@ -669,6 +669,7 @@ struct Values {
     // Tweaks
     SwitchableSetting<bool> reduce_downcount_slice{false, "reduce_downcount_slice"};
     SwitchableSetting<bool> priority_boost_starved_threads{true, "priority_boost_starved_threads"};
+    SwitchableSetting<bool> use_fastmem{true, "use_fastmem"};
 
     // Video Dumping
     std::string output_format;
