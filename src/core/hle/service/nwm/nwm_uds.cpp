dbe793ea7423faec6a19dc59b61acb54b41f9e12
peraltaklark <peraltaklark@gmail.com>
2026-08-05 01:27:28 +0800

    Increase CONNECTION_TIMEOUT to 60 seconds

--- src/core/hle/service/nwm/nwm_uds.cpp
+++ src/core/hle/service/nwm/nwm_uds.cpp
@@ -58,7 +58,7 @@ constexpr std::size_t MaxBeaconFrames = 15;
 constexpr u16 BroadcastNetworkNodeId = 0xFFFF;
 
 constexpr std::chrono::milliseconds HEARTBEAT_INTERVAL{500};
-constexpr std::chrono::milliseconds CONNECTION_TIMEOUT{20000};
+constexpr std::chrono::milliseconds CONNECTION_TIMEOUT{60000};
 constexpr std::chrono::milliseconds RECONNECT_DELAY{2000};
 constexpr int MAX_RECONNECT_ATTEMPTS = 3;
 constexpr u8 HEARTBEAT_CHANNEL = 0xF;
