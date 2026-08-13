055a0096504c6c30af15309314840fb378723633
peraltaklark <peraltaklark@gmail.com>
2026-08-13 15:33:38 +0800

    Increase MAX_BATCH_SIZE from 1 to 32

--- src/video_core/renderer_vulkan/vk_rasterizer.h
+++ src/video_core/renderer_vulkan/vk_rasterizer.h
@@ -128,7 +128,7 @@ private:
         }
     };
 
-    static constexpr size_t MAX_BATCH_SIZE = 1;
+    static constexpr size_t MAX_BATCH_SIZE = 32;
 
     std::vector<DrawBatchEntry> draw_batch;
     bool batch_active = false;
