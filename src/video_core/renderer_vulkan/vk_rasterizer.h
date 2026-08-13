fdd8cffba93ef72811b6282ddced1ee4d578acb5
peraltaklark <peraltaklark@gmail.com>
2026-08-13 08:09:51 +0800

    Change MAX_BATCH_SIZE from 128 to 1

--- src/video_core/renderer_vulkan/vk_rasterizer.h
+++ src/video_core/renderer_vulkan/vk_rasterizer.h
@@ -128,7 +128,7 @@ private:
         }
     };
 
-    static constexpr size_t MAX_BATCH_SIZE = 128;
+    static constexpr size_t MAX_BATCH_SIZE = 1;
 
     std::vector<DrawBatchEntry> draw_batch;
     bool batch_active = false;
